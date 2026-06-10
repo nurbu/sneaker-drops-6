869fd7390513fc2c175ccd1860a6ee9c38190651
Nurbu <lamanurbu2@gmail.com>
2026-06-10 11:43:13 -0400

    Added the files from the previous version

--- src/main/java/com/pluralsight/sneakerdrops/SneakerDrops6Application.java
+++ src/main/java/com/pluralsight/sneakerdrops/SneakerDrops6Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SneakerDrops6Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SneakerDrops6Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerDrops6Application.class, args);
+    }
 
 }
--- src/test/java/com/pluralsight/sneakerdrops/SneakerDrops6ApplicationTests.java
+++ src/test/java/com/pluralsight/sneakerdrops/SneakerDrops6ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SneakerDrops6ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
